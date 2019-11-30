cac1536ee9fc4b9c1501154f2cb7955c1c55599e
Tali <talisch@post.bgu.ac.il>
2019-11-30 18:47:48 +0200

    indexer - done.

--- Sources/Part_1/Indexer.java
+++ Sources/Part_1/Indexer.java
@@ -1,29 +1,29 @@
 package Part_1;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
 public class Indexer {
 
-
-    static HashMap<String, int[]> termDic = new HashMap<>(); // 0 -df, 1- tf, list of docs ???
-    private int currNum = -1;
-    private int termNumOfDocs = -1;
+    // static HashMap<String, int[]> termDic = new HashMap<>(); // 0 -df, 1- tf, list of docs ???
     private String currDocName = "";
     private Document currDoc;
-    private String currTerm;
     private HashMap<String, int[]> currDocDic = new HashMap<>();
-    private static HashMap<String, Integer> allTermsDF = new HashMap<>(); //df !
-    private HashMap<String, Queue<String>> allTermsInDocs = new HashMap<>(); //which docs !
-
+    private String FILE_PATH = "";
 
     private void indexAll(Queue<Document> q_docs) {
 
-        Queue<String> oldAllDocs = null;
-        Queue<String> newAllDocs = null;
+        FILE_PATH = "C:\\Users\\Tali\\IdeaProjects\\SearchEngine\\out\\dictionary.txt";
+        Path path = Paths.get(FILE_PATH);
 
         while (q_docs != null && !q_docs.isEmpty()) { //indexes all docs in the queue (CHUNK)
             currDoc = q_docs.poll();
@@ -31,50 +31,45 @@ public class Indexer {
             currDocName = currDoc.getId();
 
             for (String key : currDocDic.keySet()) {
-                //int[] values = currDocDic.get(key); // not neccesary.
-                if (termDic.containsKey(key)) { //this term already exists
-                    termNumOfDocs = allTermsDF.get(key); //old value of # docs for term
-                    allTermsDF.replace(key, termNumOfDocs, termNumOfDocs + 1); //adds one more doc to count.
-                    oldAllDocs = allTermsInDocs.get(key);
-                    newAllDocs = oldAllDocs; ////// ////// pointerrrr problemmmm
-                    newAllDocs.add(currDocName); //adds curr doc to list of docs for this term
-                    allTermsInDocs.replace(key, oldAllDocs, newAllDocs); //updates the map
-                } else {
-                    termDic.put(key, null);
-                    allTermsDF.put(key, 1); //first doc...
-                    Queue<String> firstDocToList = new LinkedList<>() ;
-                    firstDocToList.add(currDocName);
-                    allTermsInDocs.put(key, firstDocToList);
+
+                boolean exists = false;
+                try{
+                    Scanner textScan = new Scanner(new File(FILE_PATH));
+
+                    while (textScan.hasNextLine() && !exists) {
+                        String str = textScan.nextLine();
+                        if (str.indexOf(key) != -1) { //word already exists in dic (posting, txt file)
+
+                            int currTF = currDocDic.get(key)[0]; //num of appearences in specific doc. !!!!!!!!!!!
+                            String docToAdd = str + ", " + currDocName + "-" + currTF; //// d2-7, (times in doc)
+
+                            List<String> fileContent = new ArrayList<>(Files.readAllLines(path, StandardCharsets.UTF_8));
+
+                            for (int i = 0; i < fileContent.size(); i++) {
+                                if (fileContent.get(i).equals(str)) {
+                                    fileContent.set(i,docToAdd);
+                                    break;
+                                }
+                            }
+                            Files.write(path, fileContent, StandardCharsets.UTF_8);
+                            exists = true;
+                        }
+                    }
+                    if (!exists) //new term needs to be added
+                        try {
+                            int currTF = currDocDic.get(key)[0]; //num of appearences in specific doc. !!!!!!!!!!!
+                            String docToAdd = key + ", 1, " + currDocName + "-" + currTF; ////Abba - 1, d2-7, (times in doc)
+                            Files.write(path, docToAdd.getBytes(), StandardOpenOption.APPEND);
+                        }catch (IOException e) {
+                            //exception handling left as an exercise for the reader
+                        }
+                } catch (IOException ex) {
+                    // Report
                 }
-                //System.out.println("Key = " + key + ", Value = " + values);
             }
         }
     }
 
-
-
-
-    private void addTerm (String term) {
-
-        boolean exists = false;
-        try{
-            Scanner textScan = new Scanner(new File("/out/dictionary.txt"));
-
-            while (textScan.hasNextLine() && !exists) {
-                String str = textScan.nextLine();
-                if (str.indexOf(term) != -1) { //word already exists in dic
-                    updatePost(term); //////////// ??????????????????? or here ? dont need another functiion ?
-                    exists = true;
-                }
-            }
-            if (!exists) //new term needs to be added
-                addNewTerm(term);
-
-        } catch (IOException ex) {
-            // Report
-        }
-
-    }
 
     private void createFile (String terms) {
         Writer writer = null;
@@ -113,10 +108,6 @@ public class Indexer {
         addNewTerm("Abba, tf = 2, idf = 3.");
     }
 
-    public void search (){
-        addTerm("Abba");
-    }
-
     public void test (){
         Queue<Document> docs = new LinkedList<Document>();
         HashMap<String, int[]> tDic = new HashMap<>();
@@ -125,9 +116,20 @@ public class Indexer {
         Document d2 = new Document();
         Document d3 = new Document();
 
+        int[] test = new int[3];
+        test[0] = 1;
+        test[1] = 0;
+        test[2] = 2;
+
         StringBuilder sb = new StringBuilder();
-        tDic.put("Abba", null);
-        tDic.put("Ima", null);
+        tDic.put("Abba", test);
+
+        int[] test2 = new int[3];
+        test2[0] = 4;
+        test2[1] = 0;
+        test2[2] = 2;
+
+        tDic.put("Ima",test2);
 
         d1.setId("FB396005");
         d1.setText(sb);
@@ -137,9 +139,9 @@ public class Indexer {
         StringBuilder sb2 = new StringBuilder();
         HashMap<String, int[]> tDic2 = new HashMap<>();
 
-        tDic2.put("Ima" , null);
-        tDic2.put("Abba", null);
-        tDic2.put("Tomer", null);
+        tDic2.put("Ima" , test);
+        tDic2.put("Abba", test);
+        tDic2.put("Tomer", test);
 
         d2.setId("SW396938");
         d2.setText(sb2);
@@ -149,9 +151,9 @@ public class Indexer {
         StringBuilder sb3 = new StringBuilder();
         HashMap<String, int[]> tDic3 = new HashMap<>();
 
-        tDic3.put("Tomer" , null);
-        tDic3.put("Abba", null);
-        tDic3.put("Tomer", null);
+        tDic3.put("Tomer" , test);
+        tDic3.put("Abba", test);
+        tDic3.put("Tomer", test);
 
         d3.setId("TA834655");
         d3.setText(sb3);
@@ -162,18 +164,15 @@ public class Indexer {
         docs.add(d2);
         docs.add(d3);
 
-        int[] test = new int[3];
-        test[0] = 1;
-        test[1] = 0;
-        test[2] = 2;
 
-        termDic.put("Abba", test);
-        termDic.put("Ima", test);
+
+     //   termDic.put("Abba", test);
+     //   termDic.put("Ima", test);
       //  termDic.put("tomer", test);
        // termDic.put("Tomer", test);
 
-        allTermsDF.put("Ima", 3);
-        allTermsDF.put("Abba", 4);
+        //allTermsDF.put("Ima", 3);
+       // allTermsDF.put("Abba", 4);
        // allTermsDF.put("Tomer", 1);
 
         Queue<String> allDocs = new LinkedList<>();
@@ -181,14 +180,14 @@ public class Indexer {
         ((LinkedList<String>) allDocs).add(1, "d2");
         ((LinkedList<String>) allDocs).add(2, "d3");
 
-        allTermsInDocs.put("Ima", allDocs);
-        Queue<String> allDocs2 = new LinkedList<>();
-        ((LinkedList<String>) allDocs2).add(0, "d1");
-        ((LinkedList<String>) allDocs2).add(1, "d2");
+     //   allTermsInDocs.put("Ima", allDocs);
+     //   Queue<String> allDocs2 = new LinkedList<>();
+     //   ((LinkedList<String>) allDocs2).add(0, "d1");
+      //  ((LinkedList<String>) allDocs2).add(1, "d2");
 
-        ((LinkedList<String>) allDocs2).add(2, "d3");
-        ((LinkedList<String>) allDocs2).add(3, "d4");
-        allTermsInDocs.put("Abba", allDocs2);
+       // ((LinkedList<String>) allDocs2).add(2, "d3");
+      //  ((LinkedList<String>) allDocs2).add(3, "d4");
+      //  allTermsInDocs.put("Abba", allDocs2);
 
 
 
